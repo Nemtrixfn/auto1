4d4b626a42db99f68108b42db4e03cf6f5df6f1d
Jakub Sedláček <Sedlacek.Jakub@stud.vassboskovice.cz>
2022-03-15 11:36:19 +0100

    Přidejte soubory projektu.

---
+++ auto1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace auto1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Vehicle auto1;
+        Vehicle auto2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            auto1 = new Vehicle("Škoda");            
+            auto1.Type = "Osobní auto";
+            ShowVehicle(auto1, txt1); 
+            auto2 = new Vehicle("WM");            
+            auto2.Type = "Dodávka";
+            ShowVehicle(auto2, txt2);
+        }
+
+        public void ShowVehicle(Vehicle vehicle, TextBox textBox)
+        {
+            textBox.Text = $"brand: { vehicle.Brand}\n";
+            textBox.Text += $"Type: {vehicle.Type}\n";
+            textBox.Text += $"Fuel: {vehicle.Fuel}\n";
+            textBox.Text += $"EngineVolume: {vehicle.EngineVolume}\n";
+            textBox.Text += $"FuelLevel: {vehicle.FuelLevel}\n";
+            textBox.Text += $"Distance: {vehicle.Distance}\n";
+            textBox.Text += $"PneuState: {Math.Round(vehicle.PneuState,2)}\n";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            auto1.Move();
+            auto2.Move();
+            ShowVehicle(auto1, txt1);
+            ShowVehicle(auto2, txt2);
+        }
+    }
+}
---
+++ auto1/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace auto1
+{
+    public class Vehicle
+    {
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public string Fuel { get; set; }
+        public double EngineVolume { get; set; }
+        public int FuelLevel { get; set; }
+        public int Distance { get; set; }
+        public double PneuState { get; set; }
+
+        public Vehicle(string brand)
+        {                                                                                                                                                                 
+            Brand = brand;
+            Fuel = "Gas";
+            EngineVolume = 1000;
+            FuelLevel = 100;
+            Distance = 0;
+            PneuState = 100;
+        }
+        
+        public void Move()
+        {
+            FuelLevel -= 1;
+            Distance += 20;
+            PneuState -= 0.01;
+        }
+        
+
+        
+    }
+}
